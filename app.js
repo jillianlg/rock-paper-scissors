3b06ba7608d33694b53da52eca236b7d3a621e34
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-09-30 15:37:55 -0700

    add event listeners

--- app.js
+++ app.js
@@ -5,10 +5,30 @@ const checkedRadio = document.querySelector(':checked');
 const button = document.querySelector('#play-button');
 const resultsSpan = document.querySelector('#result-message');
 const winSpan = document.querySelector('#win-tracker');
-const loseSpan = document.querySelector('#loss-tracker');
+const lossesSpan = document.querySelector('#loss-tracker');
 const drawSpan = document.querySelector('#draw-tracker');
 
-console.log(checkedRadio, button, resultsSpan, winSpan, loseSpan, drawSpan);
 // initialize state
+let wins = 0;
+let losses = 0;
+let draw = 0;
 
 // set event listeners to update state and DOM
+button.addEventListener('clicked', () => {
+    const getRandomThrow = Math.ceil(Math.random() * 3);
+  
+    let throwIs;
+    // if getRandomThrow is 1, it's rock
+    if (getRandomThrow === 1){
+        throwIs = 'rock';
+    }
+    // if getRandomThrow is 2, it's paper
+    else if (getRandomThrow === 2){
+        throwIs = 'paper';
+    }
+    // if getRandomThrow is 3, it's sissors
+    else if (getRandomThrow === 3){
+        throwIs = 'scissors';
+    }
+    return throwIs;
+});
