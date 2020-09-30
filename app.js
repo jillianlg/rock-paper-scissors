cebc5306d3a4bc67c0d654a5c00f981c3f6e425d
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-09-30 15:08:34 -0700

    add vars to html elements in app.js and validate

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 // import functions and grab DOM elements
+// import {randomNumber } from './utils.js';
 
+const checkedRadio = document.querySelector(':checked');
+const button = document.querySelector('#play-button');
+const resultsSpan = document.querySelector('#result-message');
+const winSpan = document.querySelector('#win-tracker');
+const loseSpan = document.querySelector('#loss-tracker');
+const drawSpan = document.querySelector('#draw-tracker');
+
+console.log(checkedRadio, button, resultsSpan, winSpan, loseSpan, drawSpan);
 // initialize state
 
 // set event listeners to update state and DOM
