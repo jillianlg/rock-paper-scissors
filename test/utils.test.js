1a1fe8c6a4c4311283cdf4595593fe90d9d94d41
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-10-01 16:34:07 -0700

    add whoWon test to utils.test.js, ran test and passed for win, lose and draw

--- test/utils.test.js
+++ test/utils.test.js
@@ -3,7 +3,7 @@ import { getRandomThrow, whoWon } from '../utils.js';
 
 const test = QUnit.test;
 
-test('should randomly throw rock, papar or scissors', (expect) => {
+test('getRandomThrow should randomly throw rock, papar or scissors', (expect) => {
     //Arrange
     // Set up your arguments and expectations
 
@@ -20,3 +20,23 @@ test('should randomly throw rock, papar or scissors', (expect) => {
 // expect.equal(typeof actual, 'string');
 
 //Add test for whoWon function
+
+test('whoWon should return trues paper beats rock', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = whoWon('rock', 'paper');
+    const expected = 'lose';
+
+    const actual2 = whoWon('rock', 'scissors');
+    const expected2 = 'win';
+
+    const actual3 = whoWon('rock', 'rock');
+    const expected3 = 'draw';
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected, actual2, expected2, actual3, expected3);
+
+});
